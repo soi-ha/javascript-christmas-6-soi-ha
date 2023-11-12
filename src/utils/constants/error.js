44b873ef16a843bba6a816d5d9b07bda60fc098f
soi-ha <soy2302ten@gmail.com>
2023-11-13 01:27:14 +0900

    chore(OrderValidator): 음료만 주문, 수량 제한 예외 추가

---
+++ src/utils/constants/error.js
@@ -0,0 +1,6 @@
+const ERROR = Object.freeze({
+  readDate: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  readOrder: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+});
+
+export default ERROR;
--- src/utils/validators/OrderValidator.js
+++ src/utils/validators/OrderValidator.js
@@ -1,4 +1,5 @@
 import MENU from '../constants/string.js';
+import ERROR from '../constants/error.js';
 
 export class OrderValidator {
   #order;
@@ -13,25 +14,52 @@ export class OrderValidator {
     const REGEX = /^[가-힣]+-\d+$/;
     const filterOrder = order.filter((menu) => REGEX.test(menu));
     if (filterOrder.length !== order.length) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(ERROR.readOrder);
     }
   }
 
   #validateOverlapping(newFormatOrder, food) {
     if (Object.keys(newFormatOrder).includes(food)) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(ERROR.readOrder);
     }
   }
 
   #validateCheckQuantity(quantity) {
     if (quantity < 1) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(ERROR.readOrder);
     }
   }
 
   #validateExistMenu(food) {
     if (!Object.keys(MENU).includes(food)) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(ERROR.readOrder);
+    }
+  }
+
+  #validateQuantityExceed(newFormatOrder) {
+    let totalQuantity = 0;
+    const arrayOrder = Object.entries(newFormatOrder);
+    for (let i = 0; i < arrayOrder.length; i++) {
+      totalQuantity += arrayOrder[i][1];
+    }
+    if (totalQuantity > 20) {
+      throw new Error(ERROR.readOrder);
+    }
+  }
+
+  #validateOnlyBeverage(newFormatOrder) {
+    const beverageKeys = Object.keys(MENU).filter(
+      (key) => MENU[key][1] === '음료',
+    );
+    let beverageCount = 0;
+    beverageKeys.map((beverage) => {
+      if (Object.keys(newFormatOrder).includes(beverage)) {
+        beverageCount += 1;
+      }
+    });
+
+    if (beverageCount === Object.keys(newFormatOrder).length) {
+      throw new Error(ERROR.readOrder);
     }
   }
 
@@ -44,6 +72,8 @@ export class OrderValidator {
       this.#validateExistMenu(food);
       newFormatOrder[food] = parseInt(quantity);
     });
+    this.#validateOnlyBeverage(newFormatOrder);
+    this.#validateQuantityExceed(newFormatOrder);
     return newFormatOrder;
   }
 
