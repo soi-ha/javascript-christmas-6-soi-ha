ed088e2912aed6c0720b05e89ab544270ddac7c0
soi-ha <soy2302ten@gmail.com>
2023-11-13 21:29:42 +0900

    feat(OutputView): 출력 멘트 입력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Order from './domain/Order.js';
 import VisitDate from './domain/VisitDate.js';
+import OutputView from './view/OutputView.js';
 
 class App {
   async run() {
+    OutputView.writeEventStart();
+    const date = await VisitDate();
     const order = await Order();
-    const test = await VisitDate();
+    OutputView.writeEventResult(date);
+
+    OutputView.writeOrderMenu(order);
+    OutputView.writeBeforeDiscount(-10000000);
   }
 }
 
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,11 +1,48 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  writeEventStart() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
-  // ...
+  writeEventResult(date) {
+    Console.print(
+      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+    );
+  },
+  writeOrderMenu(order) {
+    Console.print('<주문 메뉴>');
+    Object.entries(order).map((dish) =>
+      Console.print(`${dish[0]} ${dish[1]}개`),
+    );
+  },
+  writeBeforeDiscount(before) {
+    Console.print('<할인 전 총주문 금액>');
+    Console.print(`${currencyUnit(before)}원`);
+  },
+  writeGiftMenu(gift) {
+    Console.print('<증정 메뉴>');
+    Console.print(`${gift}`);
+  },
+  writeBenefitDetail(benefit) {
+    Console.log('<혜택 내역>');
+    Console.log(`${benefit}`);
+  },
+  writeBenefitAmount(benefitAmount) {
+    Console.print('<총혜택 금액>');
+    Console.print(`${currencyUnit(benefitAmount)}원`);
+  },
+  writeAfterDiscount(after) {
+    Console.print('<할인 후 예상 결제 금액>');
+    Console.print(`${currencyUnit(after)}원`);
+  },
+  writeEventBadge(badge) {
+    Console.print('<12월 이벤트 배지>');
+    Console.print(`${badge}`);
+  },
+};
+
+const currencyUnit = function addCommasToNumber(money) {
+  return money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 export default OutputView;
